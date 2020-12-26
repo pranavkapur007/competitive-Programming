a3cdbd885a8f62f4bdf4a4e89b42700f331df3d0
pranavkapur007 <pranavkapur93@gmail.com>
2020-12-26 17:59:05 +0530

    Rearrange array alternatively without using extra space.

--- CompetitivePrograming/src/main/java/gfg/topicWise/arrays/RearrangeArrayAlternativelyWithoutExtraSpace.java
+++ CompetitivePrograming/src/main/java/gfg/topicWise/arrays/RearrangeArrayAlternativelyWithoutExtraSpace.java
@@ -3,22 +3,21 @@
  * Given a sorted array of positive integers, rearrange the array alternately i.e
  * first element should be the maximum value, second minimum value, third-second max,
  * fourth-second min and so on.
- *
+ * <p>
  * Example:
  * Input: arr[] = {1, 2, 3, 4, 5, 6, 7}
  * Output: arr[] = {7, 1, 6, 2, 5, 3, 4}
- *
+ * <p>
  * Time Complexity: O(n)
- * Space Complexity: O(n)
+ * Space Complexity: O(1)
  */
-
 package gfg.topicWise.arrays;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class RearrangeArrayAlternatively {
+public class RearrangeArrayAlternativelyWithoutExtraSpace {
     public static void main(String[] args) throws IOException {
         BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
 
@@ -36,46 +35,33 @@ public class RearrangeArrayAlternatively {
             for (int i = 0; i < n; i++)
                 temp[i] = Integer.parseInt(str[i]);
 
-            RearrangeAlternate obj = new RearrangeAlternate();
-
             //calling rearrange() function
-            obj.rearrange(temp, n);
+            rearrange(temp, n);
             StringBuffer sb = new StringBuffer();
 
             //appending and printing the elements
             for (int i = 0; i < n; i++)
                 sb.append(temp[i] + " ");
             System.out.println(sb);
+
         }
     }
-}
 
-
-// } Driver Code Ends
-
-
-class RearrangeAlternate {
-
-    // This function wants you to rearrange the temp array as required
-    // and returns a StringBuffer with all elements of modified temp array
-    // appended to it.
-    // temp: input array
-    // n: size of array
-    public static void rearrange(int arr[], int n) {
-
-        // Your code here
-        int left = 0;
-        int right = n - 1;
+    private static void rearrange(int[] arr, int n) {
+        int minIndex = 0;
+        int maxIndex = n - 1;
         boolean flag = true;
-        int[] temp = arr.clone();
+        int maxElement = arr[maxIndex] + 1;
         for (int i = 0; i < n; i++) {
             if (flag) {
-                arr[i] = temp[right--];
+                arr[i] = arr[i] + (arr[maxIndex--] % maxElement) * maxElement;
             } else {
-                arr[i] = temp[left++];
+                arr[i] = arr[i] + (arr[minIndex++] % maxElement) * maxElement;
             }
             flag = !flag;
         }
+        for (int i = 0; i < n; i++) {
+            arr[i] = arr[i] / maxElement;
+        }
     }
-
 }
